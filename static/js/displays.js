810354f12d12e28b35c795de3b989df9c162c0eb
Katy <katyellenyelle@gmail.com>
2023-10-19 22:00:27 -0500

    Added javascript library to html

--- static/js/displays.js
+++ static/js/displays.js
@@ -6,17 +6,24 @@ let selector = d3.select("#selDataset");
 d3.json(url).then(function(data) {
     console.log(data)
 });
-//Get the options from the year columns
-//Loop through to get each year and append to the dropdown options
-// for (let i = 0; i < years.length; i++){
-//     //Check if the year is already in the dropdown
-//     let isYearPresent = selector.selectAll("option").nodes().some(option => option.value === years[i]);  
+
+//Get the options from the years
+d3.json(url).then((data) => {
+    let year = data.year;
+    console.log(year);
+
+    // Get the options from the year columns
+    // Loop through to get each year and append to the dropdown options
+    for (let i = 0; i < data.length; i++){
+    //Check if the year is already in the dropdown
+    let isYearPresent = selector.selectAll("option").nodes().some(option => option.value === year[i]);  
     
-//     //if year not already in dropdown add it
-//     if (!isYearPresent) {
-//     selector
-//             .append("option")
-//             .text(years[i])
-//             .property("value", years[i]);
-//     }
-// };
+    //if year not already in dropdown add it
+    if (!isYearPresent) {
+    selector
+            .append("option")
+            .text(year[i])
+            .property("value", year[i]);
+        };
+    };
+});
